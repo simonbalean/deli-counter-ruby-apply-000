3298876e4c17120d96ea909b9c64e8f6fa12520c
Simon Balean <simonbalean@gmail.com>
2018-10-22 17:45:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,9 +14,9 @@ katz_deli = []
   end
 end
 
- def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli, name)
+  katz_deli << "#{name}"
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 def now_serving(katz_deli)
