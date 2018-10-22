4e8b0ee97b3e1e093df41d1a2ff76cf78b84655b
Simon Balean <simonbalean@gmail.com>
2018-10-22 17:34:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     line = []
+    line_number = 1
     katz_deli.each do |line_name|
-      line << "#{line_number = 1}. #{line_name}"
+      line << "#{line_number + 1}. #{line_name}"
       line_number = line_number + 1
     end
       puts "The line is currently: #{line.join(" ")}"
