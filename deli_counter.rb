4161be71a0d5f50c8d4ddf16658a1890b69669ee
Simon Balean <simonbalean@gmail.com>
2018-10-22 17:44:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 # Write your code here.
+
 katz_deli = []
+
  def line (deli_line)
   x = "The line is currently:"
   if deli_line.length > 0 
@@ -11,10 +13,12 @@ katz_deli = []
     puts "The line is currently empty."
   end
 end
+
  def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
  def now_serving(katz_deli)
   if katz_deli.length > 0
     puts "Currently serving #{katz_deli.shift}."
