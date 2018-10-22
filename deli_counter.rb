57462a2e303752b42426e7769fbde7d9cbd79838
Simon Balean <simonbalean@gmail.com>
2018-10-22 18:16:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,6 @@
 # Write your code here.
 
 
-# MANUAL METHOD w/o <each.with_index>
-# def line(katz_deli = [])
-#   if katz_deli == []
-#     puts "The line is currently empty."
-#   else
-#     line = []
-#     line_number = 1
-#     katz_deli.each do |line_name|
-#       line << "#{line_number}. #{line_name}"
-#       line_number = line_number + 1
-#     end
-#       puts "The line is currently: #{line.join(" ")}"
-#   end
-# end
-
-
-
 def line(katz_deli = [])
   if katz_deli.empty?
     puts "The line is currently empty."
@@ -50,5 +33,19 @@ end
 
 
 
+# MANUAL METHOD w/o <each.with_index>
+# def line(katz_deli = [])
+#   if katz_deli == []
+#     puts "The line is currently empty."
+#   else
+#     line = []
+#     line_number = 1
+#     katz_deli.each do |line_name|
+#       line << "#{line_number}. #{line_name}"
+#       line_number = line_number + 1
+#     end
+#       puts "The line is currently: #{line.join(" ")}"
+#   end
+# end
 
 
